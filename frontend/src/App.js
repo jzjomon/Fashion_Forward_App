6932bd9e1a44a5adfcbba3f92c48cfe5ca1f401b
jomon jose <jzjomon@gmail.com>
2023-09-18 15:23:12 +0530

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import { Fragment } from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <Fragment>
+      <h1 className='text-danger'>Hello World</h1>
+    </Fragment>
+  )
+}
+
+export default App;
