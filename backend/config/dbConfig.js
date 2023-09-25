96f3585ac19b0c15530862637e192bee041554c7
jomon jose <jzjomon@gmail.com>
2023-09-25 23:27:42 +0530

    project name changed to turf house

--- backend/config/dbConfig.js
+++ backend/config/dbConfig.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const connectDataBase = async () => {
     try{
-        const dbConnect = await mongoose.connect('mongodb://127.0.0.1:27017/styleZone',{
+        const dbConnect = await mongoose.connect('mongodb://127.0.0.1:27017/TurfHouse',{
             useNewUrlParser: true
         })
         console.log(`MongoDB Connected: {conn.connection.host}`);
